b03bc1c149df4a0727389d44f1cb1fb31aade1de
3x0c3t <jcalonne.prv@gmail.com>
2026-09-16 18:58:04 +0200

    v1.0 - configure WiFi credentials

---
+++ WiFi.h
@@ -0,0 +1,11 @@
+#ifndef WIFI_SETTINGS_H
+#define WIFI_SETTINGS_H
+
+#include <ESP8266WiFi.h>
+
+// === WIFI SETTINGS ===
+
+const char* WIFI_SSID = "N0wifiHere";
+const char* WIFI_PASSWORD = "@PassWeurd";
+
+#endif
